0f6764794767d16ce1bd4ab38e069cc37156ac08
Louis <roccalouis26@gmail.com>
2025-02-04 22:22:00 +0100

    :zap: ajout du authGard dans le BookingController

--- server/src/booking/booking.controller.ts
+++ server/src/booking/booking.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, UseGuards } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingDTO } from './dto/booking.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('booking')
 export class BookingController {
   constructor(private bookingService: BookingService) {}
 
+  @UseGuards(AuthGuard)
   @Post('/create')
   async createBooking(
     @Body()
